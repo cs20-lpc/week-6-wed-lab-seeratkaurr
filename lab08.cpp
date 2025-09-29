ca7f9db587b88a1517a1447cdfd27a7fdef1e37c
seeratkaurr <hiiiseerat@gmail.com>
2025-09-28 20:54:46 -0700

    Update lab08.cpp

--- lab08.cpp
+++ lab08.cpp
@@ -2,105 +2,69 @@
 #include <string>
 using namespace std;
 
-/*******************************************************************************
- * Function prototypes
-*******************************************************************************/
 
-unsigned fact(unsigned);
-unsigned fib(unsigned);
-unsigned mult(unsigned, unsigned);
-unsigned power(unsigned, unsigned);
-unsigned product(unsigned, unsigned);
 
-/*******************************************************************************
- * Description:
- * Starting point of the program. Calls various recursive functions that can be
- * optimized using tail recursion.
- * 
- * Input:
- * N/A
- *
- * Output:
- * An integer to signal to the OS the exit code.
-*******************************************************************************/
+unsigned fact(unsigned n);
+unsigned fib(unsigned n);
+unsigned mult(unsigned x, unsigned y);
+unsigned power(unsigned x, unsigned y);
+unsigned product(unsigned x, unsigned y);
+
+
 
 int main() {
-    // try a ridiculous case that won't work without tail recursion
     cout << "6 * 123000 = " << mult(6, 123000) << endl;
-    
-    // these functions can't demonstrate the usefulness of tail recursion
-    // due to data type overflow, but still, good practice
     cout << "3 ^ 10 = " << power(3, 10) << endl;
     cout << "8 * 9 * ... * 15 = " << product(8, 15) << endl;
     cout << "10! = " << fact(10) << endl;
-
-    // without tail recursion, this next call is going to take a while
     cout << "50th Fibonacci number is " << fib(50) << endl;
-
-    // terminate
     return 0;
 }
 
-/*******************************************************************************
- * Function definitions below are NOT tail recursive!
- * TODO: make them tail recursive :)
-*******************************************************************************/
 
+
+
+unsigned fact_helper(unsigned n, unsigned acc) {
+    if (n <= 1) return acc;
+    return fact_helper(n - 1, n * acc);
+}
 unsigned fact(unsigned n) {
-    // base cases (combined)
-    if (n <= 1) {
-        return 1;
-    }
-
-    // recursive case
-    unsigned res = fact(n - 1);
-    return res * n;
+    return fact_helper(n, 1);
 }
 
+
+unsigned fib_helper(unsigned n, unsigned a, unsigned b) {
+    if (n == 0) return a;
+    if (n == 1) return b;
+    return fib_helper(n - 1, b, a + b);
+}
 unsigned fib(unsigned n) {
-    // base case 1
-    if (n == 0) {
-        return 0;
-    }
-
-    // base case 2
-    else if (n == 1) {
-        return 1;
-    }
-
-    // recursive case
-    return fib(n - 1) + fib(n - 2);
+    return fib_helper(n, 0, 1);
 }
 
+
+unsigned mult_helper(unsigned x, unsigned y, unsigned acc) {
+    if (y == 0) return acc;
+    return mult_helper(x, y - 1, acc + x);
+}
 unsigned mult(unsigned x, unsigned y) {
-    // base case
-    if (y == 0) {
-        return 0;
-    }
-
-    // recursive case
-    unsigned res = mult(x, y - 1);
-    return res + x;
+    return mult_helper(x, y, 0);
 }
 
+
+unsigned power_helper(unsigned x, unsigned y, unsigned acc) {
+    if (y == 0) return acc;
+    return power_helper(x, y - 1, acc * x);
+}
 unsigned power(unsigned x, unsigned y) {
-    // base case
-    if (y == 0) {
-        return 1;
-    }
-
-    // recursive case
-    unsigned res = power(x, y - 1);
-    return res * x;
+    return power_helper(x, y, 1);
 }
 
+unsigned product_helper(unsigned x, unsigned y, unsigned acc) {
+    if (x > y) return acc;
+    return product_helper(x + 1, y, acc * x);
+}
 unsigned product(unsigned x, unsigned y) {
-    // base case
-    if (x == y) {
-        return x;
-    }
-
-    // recursive case
-    unsigned p = product(x + 1, y);
-    return p * x;
+    return product_helper(x, y, 1);
 }
+
